4db90e7c80a866fe22ed46d513e375c0bc61e1e5
stanercelik <tanercelik2001@gmail.com>
2024-04-25 00:02:07 +0300

    VerticalCell - HorizontalCell UI Created

---
+++ GetirLiteBootcampFinalCase/Module/Listing/HorizontalProducts/HorizontalCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalCollectionViewCell.swift
+//  GetirLiteBootcampFinalCase
+//
+//  Created by Taner Çelik on 24.04.2024.
+//
+
+import UIKit
+
+class HorizontalCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ GetirLiteBootcampFinalCase/Module/Listing/VerticalProducts/VerticalCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalCollectionViewCell.swift
+//  GetirLiteBootcampFinalCase
+//
+//  Created by Taner Çelik on 24.04.2024.
+//
+
+import UIKit
+
+class VerticalCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
